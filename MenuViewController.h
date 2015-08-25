199408f984d2711d9d4db14e37cf115f3ad01c1c
周春鑫 <578207129@qq.com>
2015-08-25 17:07:25 +0800

    滑动视图

---
+++ MenuViewController.h
@@ -0,0 +1,28 @@
+//==============================================================
+/**
+ *  使用方法
+ *
+1.设置titleArray  , vcArray , frame
+2.添加view
+ _menuvc = [[MenuViewController alloc] initViewControllerWithTitleArray:@[@"首页",@"动态"] vcArray:@[marsterVc, dynamicVc]];
+ _menuvc.view.frame = CGRectMake(0, 64, self.view.frame.size.width, [UIScreen mainScreen].bounds.size.height - 64 - 49);
+ [self.view addSubview:_menuvc.view];
+ */
+//==============================================================
+
+
+#import <UIKit/UIKit.h>
+
+@protocol MenuViewControllerDelegate <NSObject>
+
+-(void)menuViewController:(id)vc currentPageChanged:(NSInteger)pageIndex;
+
+@end
+
+@interface MenuViewController : UIViewController
+@property(nonatomic,assign) id<MenuViewControllerDelegate>delegate;
+
+-(id)initWithFrame:(CGRect)frame titles:(NSArray*)titleArray vcArray:(NSArray *)vcArray;
+
+-(id)initViewControllerWithTitleArray:(NSArray *)titleArray vcArray:(NSArray*)vcArray;
+@end
